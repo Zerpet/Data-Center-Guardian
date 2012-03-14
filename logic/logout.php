05609f9fa6fd0e39cc845be6aa075602c8d4f7de
zerpet <ipodaitortbz@gmail.com>
2012-03-14 14:54:32 +0100

    Fixed URL in Location: redirection

--- logic/logout.php
+++ logic/logout.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     session_destroy();
-    header("Location: https//163.117.142.145/pfc/index.php");
+    header("Location: https://163.117.142.145/pfc/index.php");
 ?>
